622e630de10c0aae518a7aa5f3820352407760eb
kohidev <kohidev2010@gmail.com>
2025-04-24 15:15:06 +0700

    add a file output  variables

---
+++ Output_Variables.py
@@ -0,0 +1,21 @@
+"""x = "python is awesome"
+print(x)
+"""
+"""x = "python"
+y = "is"
+z = "awesome"
+print(x,y,z)"""
+"""x = "python"
+y = "is"
+z = "awesome"
+print(x+y+z)"""
+"""x = 5
+y = 10
+print(x + y)"""
+#this command is incorrect. because it's impossible to combine between string and int
+"""x = 5
+y = "John"
+print(x + y)"""
+"""x = 5
+y = "John"
+print(x,y)"""
