a392095a617753f5856963fb0b46593cfe6100b6
kohidev <kohidev2010@gmail.com>
2025-04-27 15:47:46 +0700

    create and modify escape_character.py

---
+++ Python_strings/escape_character.py
@@ -0,0 +1,17 @@
+"""txt = "we are the so-called \"vikings\" from the north"
+print (txt)"""
+
+"""txt = "this is insert one \\(backslash)"
+print(txt)"""
+
+"""txt = "Hello\nWorld"
+print(txt)"""
+
+"""txt = "hello\rworld"
+print(txt)"""
+
+"""txt = "hello\tworld"
+print(txt)"""
+
+"""txt = "hello \bworld"
+print(txt)"""
