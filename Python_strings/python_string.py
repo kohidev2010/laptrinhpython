ce3f8a6ac074fbd76c1b702db524acb8ca0aa4db
kohidev <kohidev2010@gmail.com>
2025-04-27 11:03:31 +0700

    them file python variable

---
+++ Python_strings/python_string.py
@@ -0,0 +1,32 @@
+"""b = "Hello World"
+print(b)
+"""
+
+"""print("it's alright!")
+print("he is called \"Johny\"")
+"""
+"""a = "hello jdgj jdjhfjd jdhjfd sjdfhjd jdfj"
+print(a)"""
+
+"""a = "hello world"
+print(a[0])"""
+
+"""for x in "banana":
+    print(x)"""
+
+"""a = "Hello world"
+print(len(a))"""
+
+"""text = "the best things in life are free"
+print ("free" in text)"""
+
+"""text = "the best things in life are free"
+if "free" in text:
+    print ("yes, \'free\' is present. ")"""
+
+"""text = "the best things in life are free"
+print("expensive" not in text)"""
+
+text = "the best things in life are free"
+if "expensive" not in text:
+    print("No, \'expensive\' is not present")
