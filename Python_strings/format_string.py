edf4a520a0174c589eff8cbbe2bf94275a452dba
kohidev <kohidev2010@gmail.com>
2025-04-27 15:33:00 +0700

    create and modify format_string.py

---
+++ Python_strings/format_string.py
@@ -0,0 +1,20 @@
+"""age = 36
+txt = "My name is John, i'm " + age
+print(txt)"""
+#=> these incorrect combine
+
+"""age = 36
+txt = f"my name is John, i am {age}"
+print(txt)"""
+
+"""price = 59
+txt = f"the price is {price} dollars"
+print(txt)"""
+
+"""price = 59
+txt = f"the price is {price:.2f} dollars"
+print(txt)"""
+
+
+txt = f"the price is {20 * 59} dollars "
+print (txt)
