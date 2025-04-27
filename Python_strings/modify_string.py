324fa5ebc7b3850bb67d9864db37200147f64381
kohidev <kohidev2010@gmail.com>
2025-04-27 14:48:53 +0700

    create and modify modify_string.py

---
+++ Python_strings/modify_string.py
@@ -0,0 +1,15 @@
+"""a = "hello world"
+print(a.upper())"""
+
+"""a = "Hello World"
+print(a.lower()
+      )"""
+
+"""a = " hello, world "
+print(a.strip())"""
+
+"""a = "hello world"
+print(a.replace("h","j"))"""
+
+a = "hello, world"
+print(a.split(","))
