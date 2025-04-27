13c2440f9f083c06ffe013e6bf9e1e49c48f134c
kohidev <kohidev2010@gmail.com>
2025-04-27 11:13:50 +0700

    create a folder named Python_variables

---
+++ Python_variables/Assign_Multiple_Values.py
@@ -0,0 +1,9 @@
+#x, y, z = "orange", 13, "Cherry"
+#print(x,y,z)
+"""x = y = z = "orange"
+print (x)
+print (y)
+print (z)"""
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print (x, y, z)
