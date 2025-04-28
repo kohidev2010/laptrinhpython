caa00a4e57a205d8ec47fae9272b6c8ae7212edf
kohidev <kohidev2010@gmail.com>
2025-04-28 08:56:08 +0700

    modify string_method.py

--- Python_strings/string_method.py
+++ Python_strings/string_method.py
@@ -108,3 +108,72 @@ if (userN is True):
         print("please enter again. your password is not reasonable")
 else:
     print("username is not correct, username only must contain alphabetic. please try again")"""
+
+#isdigit()--------------------------------------
+#Ngoài số 0–9, còn hỗ trợ: số mũ, số nhỏ (ví dụ: ², ³) hoặc chữ số trong các ngôn ngữ khác
+"""txt = "50800"
+x = txt.isdigit()
+print(x)"""
+
+#isidentifier()--------------------------------------
+#Nó kiểm tra xem chuỗi đó có phải là một "tên hợp lệ" (identifier) trong Python hay không
+#Chỉ chứa chữ cái (a-z, A-Z), số (0-9), hoặc dấu gạch dưới _.
+#Không được bắt đầu bằng số.
+#Không chứa dấu cách, ký tự đặc biệt (@, #, !,...) hoặc ký tự không hợp lệ.
+"""a = "MyFolder"
+b = "Demo002"
+c = "2bring"
+d = "my demo"
+print(a.isidentifier())
+print(b.isidentifier())
+print(c.isidentifier())
+print(d.isidentifier())"""
+
+#lower()--------------------------------------
+#Check if all the characters in the text are in lower case:
+"""txt = "hello world"
+x = txt.islower()
+print(x)"""
+
+"""a = "Hello world!"
+b = "hello 123"
+c = "mynameisPeter"
+
+print(a.islower())
+print(b.islower())
+print(c.islower())"""
+
+
+#isnumeric()--------------------------------------
+#Nó kiểm tra xem tất cả ký tự trong chuỗi có phải là số (số tự nhiên) hay không.
+#Nếu tất cả ký tự là số ➔ trả về True.
+#Nếu có ký tự nào không phải số ➔ trả về False.
+"""s1 = "12345"
+print(s1.isnumeric())"""#true
+"""s2 = "123abc"
+print(s2.isnumeric()) """#false
+
+#isnumeric()	Cực kỳ rộng: 0–9, phân số, số La Mã, Unicode số	"Ⅻ", "⅕"	Rộng nhất
+#isdecimal()	Chỉ số 0–9 thuần túy	"123"	Nghiêm ngặt nhất
+#isdigit()	Số 0–9 và thêm các số Unicode như ², ³	"²"	Linh hoạt hơn
+"""Tóm lại:
+Nếu bạn chỉ cần kiểm tra số 0–9 bình thường: dùng isdecimal().
+Nếu cần kiểm tra thêm các số Unicode như mũ nhỏ: dùng isdigit().
+Nếu muốn kiểm tra kể cả số La Mã, phân số Unicode...: dùng isnumeric()."""
+
+#isprintable()--------------------------------------
+#Nó kiểm tra xem mọi ký tự trong chuỗi có phải là ký tự in được (có thể hiển thị ra màn hình) hay không
+#Nếu tất cả các ký tự in được ➔ trả về True.
+#Nếu có ký tự không in được (ví dụ: xuống dòng \n, tab \t, ký tự điều khiển...) ➔ trả về False
+"""s1 = "hello world"
+print(s1.isprintable())"""#True
+"""s2 = "Hello\nworld"
+print(s2.isprintable())"""#False
+"""s3 = "12345"
+print(s3.isprintable())"""#True
+"""s4 = " "
+print(s4.isprintable())#True(khoang trang van in duoc)
+"""
+"""s5 = "hello\tworld"
+print(s5.isprintable())"""
+
