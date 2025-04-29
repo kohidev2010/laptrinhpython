26f82dddf3f6a375475c1035760fbedec0878ebe
kohidev <kohidev2010@gmail.com>
2025-04-29 12:38:13 +0700

    create a file name number_python.py

---
+++ number_python.py
@@ -0,0 +1,58 @@
+# there are threeb numeric types in Python
+#int, float, complex
+"""x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))"""
+
+#intergers:
+"""x = 1
+y = 3538454856485635
+z = -3764577
+print(type(x))
+print(type(y))
+print(type(z))"""
+
+'''x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))'''
+
+#Float can also be scientific numbers with an "e" to indicate the power of 10.
+"""x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))"""
+
+#Complex numbers are written with a "j" as the imaginary part:
+"""x = 3 + 5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))"""
+
+#convert from one type to another
+#x = 1 #int
+#y = 2.8  #float
+#z = 1j  #complex
+#convert from int to float
+#a = float(x)
+
+#convert from float to int 
+#b = int(y)
+
+#convert from int to complex
+"""c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))"""
