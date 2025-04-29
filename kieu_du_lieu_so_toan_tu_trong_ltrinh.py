b5258eed18da1077fd899ffbbfbd7cc7de70ea12
kohidev <kohidev2010@gmail.com>
2025-04-29 12:18:25 +0700

    them file kei du lieu so toan tu torng lap trinh

---
+++ kieu_du_lieu_so_toan_tu_trong_ltrinh.py
@@ -0,0 +1,47 @@
+#there are two data types involve to number
+# int and float
+"""a = 5e2 #int #5*10^2
+b = 10.5e2 #float #10.5*10^2
+print(b)"""
+
+# expression(biểu thứcthức)
+#a = 5 / 7 #toán tử: +-*/  toán hạn :5 và 7
+#print("%.2f"%(a))
+#a = 10 % 3 chia lay du =11
+#print(a)
+"""a = -10 % 3 #=22
+print(a)"""
+
+# chia lấy nguyên
+"""a = 10 // 3 #binh thhuong chia ra kq 3.333. chia lấy nguyên còn 33
+print(a) """
+
+#lấy lũy thừa
+"""a = 10 ** 3
+print(a)"""
+
+
+
+#-----------------------------------
+#phép toán logic
+"""a = 10
+b = a != 220 #logic: ==,>,<,>=,<=, !=(khac)
+print(b)"""
+
+#and , or, not
+
+"""b = 11
+c = 11
+a = 10
+d = b > c and b > a #result ís false
+print(d)"""
+
+#not 
+"""a = 10
+b = 11
+c = a > b
+print(not(c)) """
+# not là phủ định . a > b là sai nhưng phủ định là là TRUE
+a = 10
+a /= 1  #a-=1 :9 , a*=1: 1, a/=1 :10 , a**=2: a = a ** 2
+print(a)
