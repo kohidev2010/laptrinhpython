0597a47bc0e28c63466d6ac07f5a2cd2879443b4
kohidev <kohidev2010@gmail.com>
2025-04-30 12:32:01 +0700

    [200~ ham_du_lieu.py

---
+++ ham_du_lieu.py
@@ -0,0 +1,9 @@
+#hàm nhập dữ liệu trong python----------------
+#input("nhập vào tên của bạn:")
+
+# hàm import trong python
+"""import math #import ten_thu_vien as bi danh
+a = math.sqrt(16)# ten_thu_vien.ten_ham(danh sach doi so)
+print(a)"""
+
+
