db93b8cd4843b55cfb4fa85a8eef1f4f13d895b0
kohidev <kohidev2010@gmail.com>
2025-04-30 12:48:58 +0700

    mot_so_btap_van_dung_tinh_toan_co_ban.py

---
+++ mot_so_btap_van_dung_tinh_toan_co_ban.py
@@ -0,0 +1,13 @@
+#1. viết chương trình python tính thể tích hình cầu
+#2. viết chương trình python tính thể tích của hình lập phương
+#1
+#import math
+#r = int(input("nhập bán kính r: "))
+#V = 4/3 * math.pi * math.pow(r,3)
+#print("thể tích của bán kính R là: {}".format(round(V,2)))
+
+#2
+import math
+a = int(input("nhập đọ dài cạnh a: "))
+v = math.pow(a,3)
+print("thể tích hinh lap phuong là: "+str(v))
