5ea15cbe600c67a401ccf4d2b8ef9143ff840f66
kohidev <kohidev2010@gmail.com>
2025-05-01 20:27:40 +0700

    create python for loop. py

---
+++ python_for_loop.py
@@ -0,0 +1,75 @@
+#python for loop-----------------------
+"""fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)"""
+
+
+#loop through a string-----------------------
+"""for x in "banana":
+    print(x)"""
+
+
+#the break statement--------------------------
+"""fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break"""
+
+"""fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)"""
+
+
+#the continue statement--------------------
+"""fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)"""
+
+
+#the range function--------------------
+"""for x in range(6):
+    print(x)"""
+
+"""for x in range(2,6):
+    print(x)"""
+
+"""for x in range(2,30,3):
+    print(x)"""
+
+"""for x in range (6):
+    print(x)
+else:
+    print("finally finished")"""
+
+"""for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print("finally finish")"""
+
+
+#Nested Loops---------------------------
+"""adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)"""
+
+
+#the pass statement-----------------------
+"for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error."
+
+"""for x in [0, 1, 2]:
+    pass"""
+
+
+
+
+
+
