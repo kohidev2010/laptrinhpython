3058bf2e2a631cacd93ba23457075bef029a9545
kohidev <kohidev2010@gmail.com>
2025-05-03 14:29:12 +0700

    modify loop_list.py

---
+++ python_lists/loop_list.py
@@ -0,0 +1,19 @@
+#Loop through the Index numbers--------------
+# you can also loop through the list items by referring their index number
+"""use the range() and len() functions to create a suitable iterable"""
+"""thisList = ["apple", "banana", "cherry"]
+for i in range(len(thisList)):
+    print(thisList[i])"""
+
+#Using a While Loop
+"""you can loop through the list items by using a WHILE loop
+Use the len() function to determine the length of the list, then start at 0
+and loop your way through the list items by reffering to their indexes
+remember to increase the index by 1 after each iteration"""
+
+"""thisList = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thisList):
+    print(thisList[i])
+    i+=1"""
+
