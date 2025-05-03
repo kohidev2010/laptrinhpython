a016da29bac3bff1fdd59724e0f664aebda0db73
kohidev <kohidev2010@gmail.com>
2025-05-03 13:40:16 +0700

    modify remove_list_items

---
+++ python_lists/remove_list_items.py
@@ -0,0 +1,27 @@
+#Remove specified item--------------------
+"""the Remove() mothod removes specified item"""
+"""thisList = ["apple", "banana", "cherry"]
+thisList.remove("banana")
+print(thisList)"""
+#Remove() method just removes the value is specified
+#if there are more than one item with the specified value, the remove() method removes the first occurrence
+"""thisList = ["apple", "banana", "cherry", "banana", "kiwi"]
+thisList.remove("banana")
+print(thisList)"""
+
+#Remove specified Index------------------------
+#the pop() method removes the specified index
+"""thisList = ["apple", "banana", "cherry"]
+thisList.pop(1)
+print(thisList)"""
+#if you do not specify the index, the pop() method remmoves the last item
+"""thisList = ["apple", "banana", "cherry"]
+thisList.pop()
+print(thisList)"""
+
+
+#Clear the list
+"""the Clear() method emmpties the list. the list  still remains, but it has no content"""
+"""thisList = ["apple", "banana", "cherry"]
+thisList.clear()
+print(thisList)"""
