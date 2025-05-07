0d1a704ae4379df59a7248c94431b4201dd8af48
kohidev <kohidev2010@gmail.com>
2025-05-07 14:30:21 +0700

    add directory

---
+++ python_dictionaries/python_dictionaries.py
@@ -0,0 +1,75 @@
+"""thisDict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}"""
+
+#DICTIONARIES
+"""Dictionaries are used to store data values in key: value pairs"""
+"""A dictionary is a collection which is ordered, changeable, and don't allow duplicates"""
+
+"""thisDict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(type(thisDict))"""
+
+
+#Dictionaries Items
+"""
+-Dictionary items are ordered, changable, and  donn't allow duplicate
+- Dictionary items are presented in key: value pairs, and can be referred to by using the name
+"""
+
+"""thisDict = {
+    "brand": "Ford",
+    "model": "mustange",
+    "year": 1964
+}
+print(thisDict["year"])"""
+
+
+#Changeable
+"""Dictionaries are changeable, meaning that we can change, add or
+remove items after the dictinary has been created"""
+
+#Duplicates Not Allowed
+"""Dictionaries cannot have two items with the same key"""
+"""thisDict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
+}
+print(thisDict)"""
+
+
+
+#Dictionary Length
+"""To determine how manny items a dictionary has, use the len() function"""
+"""thisDict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(len(thisDict))"""
+
+
+#Dictionary Items - Data Types
+"""the values in dictionary items can be of any data type"""
+"""thisDict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "color": ["red", "white", "blue"]
+}
+print(thisDict)"""
+
+
+#the dict() Constructor
+"""it is also possible to use dict() constructor to make dictionary"""
+thisDict = dict(name = "jOhn", age = 36, country= "Norway")
+
+
+
